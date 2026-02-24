78e9fa6bc0a4846f0ca50d99aa9bb175b7d19d59
Andy <a.kolkov@gmail.com>
2026-02-24 15:32:06 +0300

    fix: coverageToRuns merged run maxValue bug + regression tests (#95)
    coverageToRuns used AlphaRuns.Add() which sets maxValue=255 for middle
    pixels in merged runs. When two adjacent pixels had identical partial
    coverage (e.g. 44% at triangle vertices), the second pixel got 100%
    instead of 44%. Fixed by using AddWithCoverage(maxValue=currentAlpha).
    
    Added 4 regression tests:
    - TestVertexCoverage_PartialAlpha: vertex pixels get correct partial alpha
    - TestEdgePosition_NoDrift: edges don't expand over distance
    - TestCoverageToRuns_MergedRunAlpha: merged runs preserve alpha
    - TestInteriorPixels_FullCoverage: interior pixels still get 100%

--- internal/raster/analytic_filler.go
+++ internal/raster/analytic_filler.go
@@ -536,7 +536,7 @@ func (af *AnalyticFiller) coverageToRuns() {
 		if alpha != currentAlpha {
 			if currentAlpha > 0 {
 				runLen := i - runStart
-				af.alphaRuns.Add(runStart, currentAlpha, runLen-1, 0)
+				af.alphaRuns.AddWithCoverage(runStart, currentAlpha, runLen-1, 0, currentAlpha)
 			}
 			currentAlpha = alpha
 			runStart = i
@@ -546,7 +546,7 @@ func (af *AnalyticFiller) coverageToRuns() {
 	// Emit final run
 	if currentAlpha > 0 {
 		runLen := af.width - runStart
-		af.alphaRuns.Add(runStart, currentAlpha, runLen-1, 0)
+		af.alphaRuns.AddWithCoverage(runStart, currentAlpha, runLen-1, 0, currentAlpha)
 	}
 }
 
